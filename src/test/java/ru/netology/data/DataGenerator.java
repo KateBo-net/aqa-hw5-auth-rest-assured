1aee8ea8725daeb7845cbab9ff90a43c8e42128a
Kate Bo <bodrakea@gmail.com>
2024-02-15 21:02:10 +0300

    init

---
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -0,0 +1,71 @@
+package ru.netology.data;
+
+import com.github.javafaker.Faker;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+import java.util.Locale;
+
+import static io.restassured.RestAssured.given;
+
+
+public class DataGenerator {
+    private static final RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+    private static final Faker faker = new Faker(new Locale("en"));
+
+    private DataGenerator() {
+    }
+
+    private static void sendRequest(RegistrationDto user) {
+        given()
+                .spec(requestSpec)
+                .body(user)
+                .when().post("/api/system/users")
+                .then().statusCode(200);
+    }
+
+    public static String getRandomLogin() {
+        var login = faker.name().username();
+        return login;
+    }
+
+    public static String getRandomPassword() {
+        var password = faker.internet().password();
+        return password;
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static RegistrationDto getUser(String status) {
+            var user = new RegistrationDto();
+            user.login = getRandomLogin();
+            user.password = getRandomPassword();
+            user.status = status;
+
+            return user;
+        }
+
+        public static RegistrationDto getRegisteredUser(String status) {
+            var registeredUser = getUser(status);
+            sendRequest(registeredUser);
+            return registeredUser;
+        }
+    }
+
+    public static class RegistrationDto {
+        String login;
+        String password;
+        String status;
+    }
+}
