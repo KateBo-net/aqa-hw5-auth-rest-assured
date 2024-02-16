9a89a7568ee361c58b37ce6026e4e1608e844f9d
Kate Bo <bodrakea@gmail.com>
2024-02-16 11:15:14 +0300

    add Lombok

--- src/test/java/ru/netology/data/DataGenerator.java
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -6,6 +6,7 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import lombok.Value;
 
 import java.util.Locale;
 
@@ -46,12 +47,7 @@ public class DataGenerator {
         }
 
         public static RegistrationDto getUser(String status) {
-            var user = new RegistrationDto();
-            user.login = getRandomLogin();
-            user.password = getRandomPassword();
-            user.status = status;
-
-            return user;
+            return new RegistrationDto(getRandomLogin(), getRandomPassword(), status);
         }
 
         public static RegistrationDto getRegisteredUser(String status) {
@@ -61,21 +57,10 @@ public class DataGenerator {
         }
     }
 
+    @Value
     public static class RegistrationDto {
         String login;
         String password;
         String status;
-
-        public String getLogin() {
-            return login;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public String getStatus() {
-            return status;
-        }
     }
 }
